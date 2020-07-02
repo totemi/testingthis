c313a3588bf836a8001b1f5685831e3fbb6d820e
Emilia <emilia@emilysapps.ltd>
2020-07-02 13:17:52 +0100

    Initial Commit

---
+++ testingthis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testingthis
+//
+//  Created by Emilia Tothova on 02/07/2020.
+//  Copyright © 2020 Emilia Tothova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
